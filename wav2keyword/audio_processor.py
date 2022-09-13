40990e18c5f2d09e53e62296778b35124405a04e
Fernando Gonzalez <fernandogonzalez512@hotmail.com>
2022-09-13 03:52:27 +0000

    added forced alignment chunking

--- wav2keyword/audio_processor.py
+++ wav2keyword/audio_processor.py
@@ -2,9 +2,9 @@
 
 # %% auto 0
 __all__ = ['model_checkpoint', 'audio_processor', 'files', 'file', 'chunk_audio', 'export_audio', 'chunk_selected_audios',
-           'AudioArray', 'AudioProcessor']
+           'align_force_align', 'parse_textgrid_items', 'parse_textgrid', 'AudioArray', 'AudioProcessor']
 
-# %% ../07_audio_processor.ipynb 2
+# %% ../07_audio_processor.ipynb 3
 from glob import glob
 import os
 from joblib import Parallel, delayed
@@ -12,6 +12,8 @@ from pathlib import Path
 from typing import List
 from pydub import AudioSegment
 import numpy as np
+import subprocess
+from praatio import textgrid, audio
 
 def chunk_audio(audio: AudioSegment, increment_mills: int = 300, windows: int = 1000) -> List[AudioSegment]:
     sample_rate = audio.frame_count(ms=1000)
@@ -32,7 +34,33 @@ def chunk_selected_audios(audios: List[str], output_path: str) -> None:
             path = os.path.join(output_path, f"{Path(file).stem}_{ix}.wav")
             export_audio(segment, path)
 
-# %% ../07_audio_processor.ipynb 3
+def align_force_align(corpus_directory: str, output_directory: str):
+    dictionary = 'english_us_arpa'
+    acoustic_model = 'english_us_arpa'
+    return subprocess.run(["mfa", "align", corpus_directory, dictionary, acoustic_model, output_directory], capture_output=True)
+
+def parse_textgrid_items(items: List[tuple], duration_seconds):
+    for ix, (k, v) in enumerate(items):
+        if 'phones' in k:
+            continue
+        for s, e, t in v.entryList:
+            start = max(s-0.1, 0)*1000
+            end = min(e+0.1, duration_seconds)*1000
+            yield start, end, t 
+
+def parse_textgrid(corpus_directory: str, output_directory: str):
+    grids = glob(os.path.join(output_directory, '*.TextGrid'))
+    assert len(grids) == 1, f"{grids}"
+    grid = grids[0]
+    name = Path(grid).stem
+    tg = textgrid.openTextgrid(grid, includeEmptyIntervals=False)
+    audio = AudioSegment.from_wav(os.path.join(corpus_directory, f"{name}.wav"))
+    parsed = [(s, e, t) for s, e, t in parse_textgrid_items(tg.tierDict.items(), audio.duration_seconds)]
+    audios = [audio[start:end] for start, end, _ in parsed]
+    transcriptions = [trans for _, _, trans in parsed]
+    return audios, transcriptions
+
+# %% ../07_audio_processor.ipynb 5
 import shutil
 from typing import List
 from collections import Counter
@@ -67,10 +95,11 @@ class AudioProcessor(object):
             max_length=int(self.feature_extractor.sampling_rate * max_duration),
             truncation=True,
             return_tensors="pt",
+            padding=True
             )
         return inputs
     
-    def parse_audio(self, raw_data: bytes, sample_width: int, channels: int, frame_rate: int) -> List[AudioSegment]:
+    def parse_raw_audio(self, raw_data: bytes, sample_width: int, channels: int, frame_rate: int) -> List[AudioSegment]:
         audio = AudioSegment.from_raw(
             BytesIO(raw_data), 
             sample_width=sample_width,
@@ -79,11 +108,25 @@ class AudioProcessor(object):
         )
         return [segment for segment in chunk_audio(audio)]
     
-    def encode_data(self, raw_data: bytes, sample_width: int, channels: int, frame_rate: int):
-        parsed = self.parse_audio(raw_data, sample_width, channels, frame_rate)
+    def encode_raw_audio(self, raw_data: bytes, sample_width: int, channels: int, frame_rate: int):
+        parsed = self.parse_raw_audio(raw_data, sample_width, channels, frame_rate)
         parsed = [{'array': np.array(p.get_array_of_samples(), dtype=float)} for p in parsed]
         parsed = [AudioArray.parse_obj(p) for p in parsed]
         return self.preprocess_function(parsed)
+    
+    def parse_aligned_audio(self, corpus_directory: str):
+        output_directory = os.path.join(Path.home(), '.cache/mfa/')
+        mfa_output = align_force_align(corpus_directory, output_directory)
+        audios, transcriptions = parse_textgrid(corpus_directory, output_directory)
+        return audios, transcriptions
+    
+    def encode_aligned_audio(self, corpus_directory: str):
+        parsed, self.transcriptions = self.parse_aligned_audio(corpus_directory)
+        parsed = [{'array': np.array(p.get_array_of_samples(), dtype=float)} for p in parsed]
+        parsed = [AudioArray.parse_obj(p) for p in parsed]
+        return self.preprocess_function(parsed)
+    
+
 
 
 model_checkpoint = 'data/panda/wav2vec2-base-finetuned-xvector/best_checkpoint/'
